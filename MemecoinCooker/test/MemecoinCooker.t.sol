99dee800c5109deee8103172d6ec390b73090471
HichRPTG <64028901+Bouananhich@users.noreply.github.com>
2024-04-10 15:23:45 +0200

    Initialize MemecoinCooker project (#4)
    * Initialize MemecoinCooker project
    
    * Remove old file
    
    * forge install: openzeppelin-contracts
    
    v5.0.2
    
    * Delete unused Counter contract and related files and created new contract + test
    
    * Add github workflows
    
    * move tests
    
    * Revert "move tests"
    
    This reverts commit 8ca3367376249e4defe17638cf6263cfab868a5a.
    
    * Update working directory for test execution
    
    * Update test command in smart-contract-tests.yml
    
    ---------
    
    Co-authored-by: Hicham Bouanani <hicham.bouanani@nuant.com>

---
+++ MemecoinCooker/test/MemecoinCooker.t.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Test, console} from "forge-std/Test.sol";
+import {MemecoinCooker} from "../src/MemecoinCooker.sol";
+
+contract MemecoinCookerTest is Test{
+    MemecoinCooker cooker;
+
+    function setup() public {
+        cooker = new MemecoinCooker();
+    }
+
+    function testMemecoinCooker() public view {
+        console.log("MemecoinCooker address: ", address(cooker));
+    }
+
+}
