efced5b4000e2082f700213ca9da4ef112e9fd8d
HichRPTG <64028901+Bouananhich@users.noreply.github.com>
2024-04-14 18:49:24 +0200

    Refactor variable name in TokenFactoryTest and MemecoinCooker.t.sol (#7)
    Co-authored-by: Hicham Bouanani <hicham.bouanani@nuant.com>

--- MemecoinCooker/src/MemecoinCooker.sol
+++ MemecoinCooker/src/MemecoinCooker.sol
@@ -2,5 +2,34 @@
 
 pragma solidity ^0.8.19;
 
+import {IFactory} from "./IFactory.sol";
+
 contract MemecoinCooker {
+
+    address _factoryContract;
+    address _owner;
+
+    constructor(address myfactoryContract) {
+        _owner = msg.sender;
+        _factoryContract = myfactoryContract;
+    }
+
+    function setFactoryContract(address myfactoryContract) public {
+        require(msg.sender == _owner, "Only owner can set factory contract");
+        _factoryContract = myfactoryContract;
+    }
+
+    function MemecoinCookerOwner() public view returns (address) {
+        return _owner;
+    }
+
+    function factoryContract() public view returns (address) {
+        return _factoryContract;
+    }
+
+    function cookMemecoin(string memory name, string memory symbol, uint256 totalSupply) public returns (address) {
+        IFactory factory = IFactory(_factoryContract);
+        return factory.issueToken(name, symbol, totalSupply, msg.sender);
+    }
+
 }
--- MemecoinCooker/test/MemecoinCooker.t.sol
+++ MemecoinCooker/test/MemecoinCooker.t.sol
@@ -4,16 +4,25 @@ pragma solidity ^0.8.19;
 
 import {Test, console} from "forge-std/Test.sol";
 import {MemecoinCooker} from "../src/MemecoinCooker.sol";
+import {TokenFactory} from "../src/TokenFactory.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 contract MemecoinCookerTest is Test{
     MemecoinCooker cooker;
+    TokenFactory factory;
 
     function setUp() public {
-        cooker = new MemecoinCooker();
+        factory = new TokenFactory();
+        cooker = new MemecoinCooker(address(factory));
+        factory.setAdminContract(address(cooker));
     }
 
-    function testMemecoinCooker() public view {
-        console.log("MemecoinCooker address: ", address(cooker));
+    function testMemecoinCooker() public {
+        address mytokenAddress = cooker.cookMemecoin("My token", "MTK", 1000);
+        bool isOwner = (factory.token2Owner(mytokenAddress) == address(this));
+        assertEq(isOwner, true, "Token ownership should be set to user");
+        bool isBalanceCorrect = (IERC20(mytokenAddress).balanceOf(address(cooker)) == 1000);
+        assertEq(isBalanceCorrect, true, "Admin should have 1000 tokens");
     }
 
 }
