922e5fd504ea3332495ef4ac085cdb718a31f0d5
Hicham Bouanani <hicham.bouanani@nuant.com>
2024-04-14 19:17:44 +0200

    Add deployment script

---
+++ MemecoinCooker/script/Myscript.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import "forge-std/Script.sol";
+import "../src/TokenFactory.sol";
+import "../src/MemecoinCooker.sol";
+
+contract MyScript is Script {
+
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        TokenFactory factory = new TokenFactory();
+        MemecoinCooker cooker = new MemecoinCooker(address(factory));
+
+        vm.stopBroadcast();
+    }
+}
